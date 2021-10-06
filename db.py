914c1c011329588aafa06df12a0f90e3b05cfc70
root <root@sh4d0w.sh4d0w>
2021-10-06 09:19:01 +0200

    Database operations finished

--- db.py
+++ db.py
@@ -1,12 +1,12 @@
-import mysql.connector
+import pymysql
 import os
 
 os.system("sudo /opt/lampp/./xampp start")
 
 
-mydb = mysql.connector.connect(
+mydb = pymysql.connect(
     host = "localhost",
-    username = "root",
+    user = "root",
     password = "",
     database = "TicTacToe"
 )
@@ -14,9 +14,20 @@ mydb = mysql.connector.connect(
 def checkPlayer(player):
     mycursor = mydb.cursor()
     query = "SELECT Username FROM Users WHERE Username = %s"
-    mycursor.execute(query , (player,))
-    for x in mycursor:
-        if mycursor(x) == player:
+    mycursor.execute(query , (player))
+    result = mycursor.fetchone()
+    '''for x in result:
+        x = int(float(x))
+        print(result[x])
+        if(result[x] == player):
+            return True
+    return False'''
+    '''
+    if(result == player):
+        return True
+    return False'''
+    for x in result:
+        if(x == player):
             return True
     return False
 
@@ -28,22 +39,40 @@ def addNewPlayer(player, win, lose, tie):
     mycursor = mydb.cursor()
     query = "INSERT INTO Users (Username, Wins, Loses, Ties) VALUES (%s, %s, %s, %s)"
     mycursor.execute(query, (player, win, lose, tie))
+    result = mycursor.fetchone()
+    mydb.commit()
 
-def updatePlayer(player, result):
-    player = player
-    result = result
+def updatePlayer(player, results):
+    help = 0
+    values = {
+        "wins" : 0,
+        "loses" : 0,
+        "ties" : 0
+    }
     mycursor = mydb.cursor()
     query = "SELECT Wins, Loses, Ties FROM Users WHERE Username = %s"
     mycursor.execute(query , (player))
-    for x in mycursor:
-        wins = mycursor[0]
-        loses = mycursor[1]
-        ties = mycursor[2]
-    if(result == "Win"):
-        wins += 1
-    elif(result == "Lose"):
-        loses += 1
-    elif(result == "Tie"):
-        ties += 1
+    result = mycursor.fetchone()
+    for x in result:
+        if(help == 0):
+            print(x)
+            values["wins"] = x
+        elif(help == 1):
+            print(x)
+            values["loses"] = x
+        elif(help == 2):
+            print(x)
+            values["ties"] = x
+        help += 1
+    if(results == "Win"):
+        values["wins"] += 1
+    elif(results == "Lose"):
+        values["loses"] += 1
+    elif(results == "Tie"):
+        values["ties"] += 1
+    wins = values["wins"]
+    loses = values["loses"]
+    ties = values["ties"]
     query = "UPDATE Users SET Wins = %s, Loses = %s, Ties = %s WHERE Username = %s"
     mycursor.execute(query , (wins, loses, ties, player))
+    mydb.commit()
